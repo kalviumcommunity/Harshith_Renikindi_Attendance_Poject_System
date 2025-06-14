1f25baa3e5811145dc2c7cfae76b3551d5a0ee29
Harshith Renikindi <harshith.renikindi@kalvium.community>
2025-06-14 12:16:02 +0530

    Session 4:Done

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -1,18 +1,28 @@
 package com.school;
 
 public class Course {
-    private static int nextCourseIdCounter = 101;
-    String courseId; // e.g., "CS101"
-    String courseName;
+    private static int nextCourseIdCounter = 101; // Start course IDs from 101 and made private
 
-    public Course(String courseName) {
-        this.courseId = nextCourseIdCounter++;
-        this.courseName = courseName;
-    
+    private int courseId; // Changed to int
+    private String courseName;
+
+    public int getCourseId(){
+        return courseId;
     }
+    
+    public String getCourseName() {
+        return courseName;
+    }
+
+
+    // Constructor
+    public Course(String courseName) {
+        this.courseId = nextCourseIdCounter++; // Auto-increment and assign ID
+        this.courseName = courseName;          // Assign course name
+    }
+
     public void displayDetails() {
         // Displaying courseId with a prefix for better readability
         System.out.println("Course ID: C" + this.courseId + ", Name: " + this.courseName);
     }
-
 }
--- src/com/school/Student.java
+++ src/com/school/Student.java
@@ -2,13 +2,23 @@ package com.school;
 
 public class Student {
     private static int nextStudentIdCounter=1;
-    int studentId;
-    String name;
-
+    private int studentId;
+    private String name;
+    
     public Student(String name){
         this.name=name;
         this.studentId=nextStudentIdCounter++;
+        }
+
+    public int getStudentId() {
+        return studentId;
     }
+
+       // Getter for name
+    public String getName() {
+        return name;
+    }
+
 
     public void displayDetails() {
         System.out.println("Student ID: " + this.studentId + ", Name: " + this.name);
